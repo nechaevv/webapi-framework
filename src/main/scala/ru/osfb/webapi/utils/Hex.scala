024efa90a217af6486f50850364577d4c8157f3d
Vasily Nechaev <vn.glider@gmail.com>
2016-05-16 03:08:00 +0300

    lowercase hex

--- src/main/scala/ru/osfb/webapi/utils/Hex.scala
+++ src/main/scala/ru/osfb/webapi/utils/Hex.scala
@@ -4,5 +4,5 @@ package ru.osfb.webapi.utils
  * Created by sgl on 20.09.15.
  */
 object Hex {
-  def apply(bytes: Array[Byte]) = bytes.map("%02X" format _).mkString
+  def apply(bytes: Array[Byte]) = bytes.map("%02x" format _).mkString
 }
