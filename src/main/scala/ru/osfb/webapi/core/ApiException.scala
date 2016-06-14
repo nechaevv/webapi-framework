f68a90104ca0bf4b83c1c50ab2f54198aa4851fe
Vasily Nechaev <vn.glider@gmail.com>
2016-06-14 13:10:30 +0300

    ApiException expose properties

--- src/main/scala/ru/osfb/webapi/core/ApiException.scala
+++ src/main/scala/ru/osfb/webapi/core/ApiException.scala
@@ -3,4 +3,4 @@ package ru.osfb.webapi.core
 /**
   * Created by sgl on 25.01.16.
   */
-class ApiException(code: String, description: String) extends RuntimeException(code)
+class ApiException(val code: String, val description: String) extends RuntimeException(code)
